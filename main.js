741b923b6f365fc51c4c7acf480fd8ded198dccd
Shawn Collette <shawn.collette@gmail.com>
2019-11-06 12:16:11 -0700

    done through 4

---
+++ main.js
@@ -0,0 +1,53 @@
+const Array = require('./Array');
+
+const main = () => {
+
+  Array.SIZE_RATIO = 3;
+
+  let arr = new Array();
+
+  console.log('2. Explore the push() method');
+  arr.push(3);
+  arr.push(5);
+  arr.push(15);
+  arr.push(19);
+  arr.push(45);
+  arr.push(10);
+  arr.push(3);
+  arr.push(5);
+  arr.push(15);
+  arr.push(19);
+  arr.push(45);
+  arr.push(10);
+  arr.push(3);
+  arr.push(5);
+  arr.push(15);
+  arr.push(19);
+  arr.push(45);
+  arr.push(10);
+  console.log(arr);
+
+  // console.log('3. Exploring the pop() method');
+  // arr.pop();
+  // arr.pop();
+  // arr.pop();
+  // console.log(arr);
+
+  // console.log('4. Understanding more about how arrays work');
+  // console.log(arr.get(0));
+
+  // arr.remove(2);
+  // arr.remove(1);
+  // arr.remove(0);
+  
+  // console.log(arr);
+  // console.log('Pushing "tauhida" to array');
+  // arr.push('tauhida');
+  // console.log(arr);
+  // console.log(arr.get(0));
+  
+  // console.log(arr);
+
+};
+
+main();
